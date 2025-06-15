5e5b50afbedaf211f10456972b84f5547064c682
szymon-koprowski <szymon.koprowski@interia.eu>
2025-06-15 10:57:29 +0200

    Edycja 3

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+bool czyPierwsza(int a){
+	if(a < 2) return false;
+	for(int i = 2; i < sqrt(a); i++){
+		if(a%i==0){
+			return false;
+		}
+	}
+	return true;
+}
 
 int main() {
     int a, b;
@@ -13,7 +24,16 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+		cout << "2. Czy pierwsza" << endl;
         cin >> wyjscie;
+		if(wyjscie == 2){
+			int x;
+			cout << "Podaj liczbe do sprawdzenia:";
+			cin >> x;
+			if(czyPierwsza(x)){
+				cout << "Liczba jest pierwsza." << endl;
+			} else cout << "Liczba nie jest pierwsza." << endl;
+		}
     } while(wyjscie != 0);
     return 0;
 }
