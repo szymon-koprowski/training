c0fe6b7eddc5efba9e13f8120f47b5a73f6ae421
szymon-koprowski <szymon.koprowski@interia.eu>
2025-06-15 10:46:03 +0200

    Edycja 2

--- projekt1.cpp
+++ projekt1.cpp
@@ -21,7 +21,14 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+		cout << "1. Silnia" << endl;
         cin >> wyjscie;
+		if(wyjscie==1){
+			cout << "Podaj liczbe:";
+			int x;
+			cin >> x;
+			cout << "Wynik: " << silnia(x) << endl;
+		}
     } while(wyjscie != 0);
     return 0;
 }
